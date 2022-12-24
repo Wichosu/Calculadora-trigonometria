6d270d85a43169c8ac942d0c5540364ba6ad9ff3
Wicho <54341712+Wichosu@users.noreply.github.com>
2022-12-23 19:32:40 -0600

    Node objects are being stored in canvasSlice

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,13 +1,46 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { 
-  add
+  add,
+  connect
 } from '../../redux/canvasSlice';
 import './Canvas.scss';
 
 const Canvas = ({ mode }) => {
   const canvasRef = useRef(null);
   const dispatch = useDispatch();
+
+  const handleMode = (e) => {
+    switch(mode){
+      case 'add':
+        dispatch(add(createNode(e)));
+        break;
+      case 'connect':
+        dispatch(connect({canvas: canvasRef.current, e}));
+        break;
+    }
+  }
+
+  const drawNode = (e) => {
+    const ctx = canvasRef.current.getContext('2d');
+    const rect = canvasRef.current.getBoundingClientRect();
+
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc(e.pageX - rect.x, e.pageY - rect.y, 8, 0, 2 * Math.PI);
+    ctx.fill()
+  }
+  
+  const createNode = (e) => {
+    const rect = canvasRef.current.getBoundingClientRect();
+
+    drawNode(e);
+
+    return {
+      x: e.pageX - rect.x,
+      y: e.pageY - rect.y
+    }
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -38,20 +71,9 @@ const Canvas = ({ mode }) => {
     }
   }, []);
 
-  // const draw = (e) => {
-  //   const canvas = canvasRef.current;
-  //   const ctx = canvas.getContext('2d');
-  //   const rect = canvas.getBoundingClientRect();
-
-  //   ctx.fillStyle = '#000000';
-  //   ctx.beginPath();
-  //   ctx.arc(e.pageX - rect.x, e.pageY - rect.y, 8, 0, 2 * Math.PI);
-  //   ctx.fill()
-  // }
-
   return (
     <div className='canvas'>
-      <canvas ref={canvasRef} onClick={(e) => dispatch(add({canvasRef, e}))} />
+      <canvas ref={canvasRef} onClick={handleMode} />
     </div>
   );
 }
--- src/redux/canvasSlice.js
+++ src/redux/canvasSlice.js
@@ -3,22 +3,20 @@ import { createSlice } from '@reduxjs/toolkit'
 export const canvasSlice = createSlice({
   name: 'canvas',
   initialState: {
-    nodes: [{}]
+    nodes: []
   },
   reducers: {
     add: (state, action) => {
-      const ctx = action.payload.canvasRef.current.getContext('2d');
-      const rect = action.payload.canvasRef.current.getBoundingClientRect();
-
-      ctx.fillStyle = '#000000';
-      ctx.beginPath();
-      ctx.arc(action.payload.e.pageX - rect.x, action.payload.e.pageY - rect.y, 8, 0, 2 * Math.PI);
-      ctx.fill()
-      state.nodes.push({});
+      state.nodes.push(action.payload);
+      alert(JSON.stringify(state.nodes));
+    },
+    connect: (state, action) => {
+      alert(JSON.stringify(state));
+      console.log(JSON.stringify(action));
     }
   }
 });
 
-export const { add } = canvasSlice.actions;
+export const { add, connect } = canvasSlice.actions;
 
 export default canvasSlice.reducer;
